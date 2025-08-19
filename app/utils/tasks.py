a23757ea7635c5d2f6ad3ef0f1beb817f4fdc527
Simon van Lierde <simonvanlierde@gmail.com>
2025-08-19 23:41:21 +0200

    refactor(tasks): improve repeat_task function to handle coroutine tasks and logging

--- app/utils/tasks.py
+++ app/utils/tasks.py
@@ -2,25 +2,38 @@
 
 import asyncio
 import logging
-from collections.abc import Awaitable, Callable
+from collections.abc import Callable, Coroutine
+from typing import Any
+
+from starlette.concurrency import run_in_threadpool
 
 logger = logging.getLogger(__name__)
 
+repeated_tasks = set()
 
-async def repeat_task(
-    task_func: Callable[[], None] | Callable[[], Awaitable[None]],
+
+def repeat_task(
+    task_func: Callable[[], None] | Callable[[], Coroutine[Any, Any, None]],
     seconds: float,
     task_name: str,
-) -> None:
-    """Repeat a task every x seconds."""
-    while True:
-        try:
-            if asyncio.iscoroutinefunction(task_func):
-                await task_func()
-            else:
-                task_func()
-            logger.info("Task '%s' executed successfully", task_name)
-        except Exception:
-            logger.exception("Exception in task '%s'", task_name)
+) -> asyncio.Task:
+    """Start a task that repeats every x seconds in the background."""
 
-        await asyncio.sleep(seconds)
+    async def _loop() -> None:
+        """Internal loop function."""
+        while True:
+            try:
+                if asyncio.iscoroutinefunction(task_func):
+                    await task_func()
+                else:
+                    await run_in_threadpool(task_func)
+                logger.info("Task '%s' executed successfully", task_name)
+            except Exception:
+                logger.exception("Exception in task '%s'", task_name)
+
+            await asyncio.sleep(seconds)
+
+    # Store tasks in a global set to avoid accidental garbage collection
+    task = asyncio.ensure_future(_loop())
+    # repeated_tasks.add(task)
+    return task
