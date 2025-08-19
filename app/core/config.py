3e693840f31c85404de9e89fb568b6d4833f9550
Simon van Lierde <simonvanlierde@gmail.com>
2025-08-19 23:42:45 +0200

    feat(logging): add custom logging setup with file and console output

--- app/core/config.py
+++ app/core/config.py
@@ -27,6 +27,7 @@ class Settings(BaseSettings):
     hls_path: Path = BASE_DIR / "data" / "hls"  # Used for storing temporary HLS video files
     image_path: Path = BASE_DIR / "data" / "images"  # Used for temporary storing of captured images
     templates_path: Path = BASE_DIR / "app" / "templates"  # Used for storing HTML templates
+    log_path: Path = BASE_DIR / "logs"  # Used for storing log files
 
     # HLS settings
     hls_manifest_filename: str = "master.m3u8"
